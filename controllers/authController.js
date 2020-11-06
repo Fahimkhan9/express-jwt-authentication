e2f134d17b0911a52adcfe9fd74d8a8cb4c8ce47
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-11-07 01:44:34 +0600

    updated mongoose validation error && created salt,hashing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,29 @@
 const User = require("../models/User");
 
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -8,13 +32,12 @@ module.exports.login_get = (req, res) => {
 };
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(404).send("error");
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
--- models/User.js
+++ models/User.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "please enter an password"],
+    minlength: [6, "minimum length is 6"],
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // console.log("user about to create", this);
+
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
